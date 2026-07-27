2bc53711c2bb9717229a53aa79ec25d98799ba27
Seydou <Skabore2020@gmail.com>
2026-07-27 23:19:25 +0000

    structure initiale du projet

---
+++ app/config.py
@@ -0,0 +1,9 @@
+ASR_MODEL_NAME = "jonatasgrosman/wav2vec2-large-xlsr-53-french"
+
+SENTIMENT_MODEL_NAME = "cmarkea/distilcamembert-base-sentiment"
+
+TARGET_SAMPLE_RATE = 16_000
+MAX_AUDIO_DURATION_SECONDS = 300 
+ALLOWED_AUDIO_EXTENSIONS = {".wav", ".mp3"}
+
+SENTIMENT_LABELS = ["negatif", "neutre", "positif"]
